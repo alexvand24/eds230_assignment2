fb25e624662a834d94d1cab8180f82edc9b90a14
Mia Forsline <miaforsline@gmail.com>
2022-04-07 18:01:33 -0700

    changed parameter names to be more specific. changed vertical organization of code to increase readability

--- almond_yield_anomaly.R
+++ almond_yield_anomaly.R
@@ -3,28 +3,33 @@
 #' This function computes almond yield anomaly 
 #’ given temperature and precipitation data. 
 #’ This function is based on a model built by Lobell et al. (2006). 
-#' @param temp minimum temperature in February (degrees C)
-#' @param precip anuary precipitation (mm)
-#' @param temp_coeff1 coefficient (ton/acre/degree C) default is -0.015
-#' @param temp_coeff2 coefficient (ton/acre/degree C) default is -0.0046
+#’
+#' @param feb_temp_min minimum temperature in February (degrees C)
+#' @param jan_precip January precipitation (mm)
+#' @param temp_coeff1 coefficient (ton/acre/degree C), default is -0.015
+#' @param temp_coeff2 coefficient (ton/acre/degree C), default is -0.0046
 #' @param precip_coeff1 coefficient (ton/acre/mm)
 #' @param precip_coeff2 coefficient (ton/acre/mm)
-#' @param constant constant (ton/acre) default is 0.28
+#' @param constant constant (ton/acre), default is 0.28
 #' @author Elmera Azadpour, Mia Forsline, Alex Vand
 #' @examples almond_yield(temp = 25, precip = 10)
 #' @return almond_yield_anomaly (ton/acre)
 
 
 
-almond_yield_anomaly = function(temp,
-                                precip,
+almond_yield_anomaly = function(feb_temp_min,
+                                jan_precip,
                                 temp_coeff1 = -0.015,
                                 temp_coeff2 = -0.0046,
                                 precip_coeff1 = -0.07,
                                 precip_coeff2 = 0.0043,
                                 constant = 0.28){
   
-  almond_yield_anomaly <- (temp_coeff1 * temp) + (temp_coeff1 * temp^2) + (precip_coeff1 * precip) + (precip_coeff2 * precip^2) + constant
+  almond_yield_anomaly <- (temp_coeff1 * temp) +
+                          (temp_coeff1 * temp^2) +
+                          (precip_coeff1 * precip) +
+                          (precip_coeff2 * precip^2) +
+                          constant
   
   return(almond_yield_anomaly)
 }

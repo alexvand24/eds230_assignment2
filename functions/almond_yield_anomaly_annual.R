f249920501dd6e027e41718ab5ecf40f7b204b40
Mia Forsline <miaforsline@gmail.com>
2022-04-07 22:27:55 -0700

    created separate R scripts for each almond anomaly function

---
+++ functions/almond_yield_anomaly_annual.R
@@ -0,0 +1,54 @@
+#' Almond yield anomaly
+#'
+#' This function computes almond yield anomaly 
+#’ given temperature and precipitation data. 
+#’ This function is based on a model built by Lobell et al. (2006). 
+#’
+#' @param feb_temp_min minimum temperature in February (degrees C)
+#' @param jan_precip January precipitation (mm)
+#' @param temp_coeff1 coefficient (ton/acre/degree C), default is -0.015
+#' @param temp_coeff2 coefficient (ton/acre/degree C), default is -0.0046
+#' @param precip_coeff1 coefficient (ton/acre/mm)
+#' @param precip_coeff2 coefficient (ton/acre/mm)
+#' @param constant constant (ton/acre), default is 0.28
+#' @author Elmera Azadpour, Mia Forsline, Alex Vand
+#' @examples almond_yield(temp = 25, precip = 10)
+#' @return almond_yield_anomaly (ton/acre)
+
+almond_yield_anomaly = function(feb_temp_min,
+                                jan_precip,
+                                temp_coeff1 = -0.015,
+                                temp_coeff2 = -0.0046,
+                                precip_coeff1 = -0.07,
+                                precip_coeff2 = 0.0043,
+                                constant = 0.28) {
+  
+  #create a temperature data frame to summarize monthly temperature minimums by year
+  temp <- clim_data %>% 
+    group_by(month, year) %>% 
+    summarize(tmin_c=mean(tmin_c))
+  
+  #extract the average minimum temperature for February 
+  tmin_feb = (temp %>% 
+                filter(month==2))$tmin_c 
+  
+  #create a precipitation data frame to summarize monthly precipitation averages by year 
+  precip <- clim_data %>% 
+    group_by(month, year) %>% 
+    summarize(precip =
+                sum(precip))
+  
+  #extract the average precipitation values for January 
+  precip_jan = (precip %>% 
+                  filter(month==1))$precip
+  
+  #calculate almond yield anomaly 
+  almond_yield_anomaly =
+    (temp_coeff1 * tmin_feb) + 
+    (temp_coeff2 * tmin_feb**2) +
+    (precip_coeff1 * precip_jan) + 
+    (precip_coeff2 * precip_jan**2) + 
+    constant
+  
+  return(almond_yield_anomaly)
+}

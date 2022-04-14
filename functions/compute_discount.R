6b6f4d8aa9cf4342ee384aba5895bbf248e7b66c
Mia Forsline <miaforsline@gmail.com>
2022-04-14 11:30:47 -0700

    added authors + examples to discount rate function roxygen2 skeleton

--- functions/compute_discount.R
+++ functions/compute_discount.R
@@ -5,10 +5,12 @@
 #' @param price/cost current price of almonds ($)
 #' @param time the future time period during which the price/cost is being calculate dfor (years)
 #' @param discount rate (a value from 0 - 1)
+#' @author Elmera Azadpour, Mia Forsline, Alex Vand
+#' @examples compute_discount(value = 10, time = 2, discount = 0.12)
 #' @return value in $
 
 
-compute_NPV = function(value, time, discount=0.12) {
+compute_NPV = function(value, time, discount = 0.12) {
   result = value / (1 + discount)**time
   return(result)
 }

217b5d4170a21325b73d611865838387950105a2
Mia Forsline <miaforsline@gmail.com>
2022-04-14 11:37:05 -0700

    annotating compute_profit function

--- functions/compute_profit_from_almonds.R
+++ functions/compute_profit_from_almonds.R
@@ -1,13 +1,19 @@
-#' computes profit from power generation
-#' @param  price ($/kj)
-#' @param  energy (kj/yr)
-#' @param  year (when was energy obtained)
-#' @param discount rate (default 0.12)
+#' compute_profit_from_almonds
+#' 
+#' This function estimates almond yield profit based on almond yield anomoly, year, price, and discount rate. 
+#' 
+#' @param  almond_yield_anomaly (ton/acre)
+#' @param  year (when was energy obtained, in the format YYYY)
+#' @param  price almond price ($/ton)
+#' @param discount rate (a value from 0 - 1)
+#' @author Elmera Azadpour, Mia Forsline, Alex Vand
+#' @examples compute
 #' @return data frame with estimate of profit
-compute_profit_frompower = function(energy, year, price, discount=0.12) {
+
+compute_profit_from_almonds = function(almond_yield_anomaly, year, price, discount=0.12) {
   
   # make sure values are reasonable
-  if (length(energy) < 1)
+  if (length(almond_yield_anomaly) < 1)
     return(NA)
   
   # energy cannot be negative
